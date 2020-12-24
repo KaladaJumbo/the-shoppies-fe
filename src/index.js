0fbe9ff7be78f9da47544f549c304626e2ec1fef
KaladaJumbo <wkalada1@gmail.com>
2020-12-24 00:05:24 -0500

    styled search bar

--- src/index.js
+++ src/index.js
@@ -2,13 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import './css/index.css';
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 
+import './css/index.css';
 import App from './App';
+
+const theme = createMuiTheme({
+
+  typography: {
+      //for any general settings to be added accross the website
+    
+  },
+  palette: {
+    primary: {
+      main: "#008060" 
+    },
+    secondary: {
+      main: "#9fffe0" //Another orange-ish color
+    }
+  },
+  // fontFamily: ""
+});
+
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from "@material-ui/core/Grid"
 import stock from "../images/stock.jpg"
-import { Typography } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
 
 
 
@@ -13,11 +14,13 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: "#fbf7ed",
         height: "66.7vh",
         width: "100%",
-        paddingTop: ".6%"
+        paddingTop: "0"
     },
     right:{
         //top section grid right
         overflow: "hidden",
+        padding: "0",
+        margin: "0",
     },
     left: {
         //top section grid left
@@ -35,7 +38,27 @@ const useStyles = makeStyles((theme) => ({
         height: "65vh",
         width: "100%",
     },
+    searchBox: {
+        borderColor: 'red',
 
+    },
+    submitButton: {
+        width: "100%", 
+        height: "40%", 
+        backgroundColor: "#008060",
+        color: "white",
+        "&:hover": {
+            backgroundColor: "#004c3f",
+        },
+    },
+    buttonStyle: {
+        color: "#004c3f",
+        borderColor: "#004c3f",
+        "&$focusedLabel": {
+            color: "#004c3f"
+          },
+
+    }
   
 }));
     
@@ -45,14 +68,49 @@ const Welcome = () => {
     return (
         <div className={classes.root}>
             <div className={classes.topsection}>
-                <Grid container xs={12} style={{padding: "0", overflow: "hidden"}}>
-                    <Grid item className={classes.left} xs={12} lg={6}>
+                <Grid container xs={12} style={{padding: "0", margin: "0", overflow: "hidden", height: "100%"}}>
+                    <Grid container item className={classes.left} xs={12} lg={6}>
                         <Typography className={classes.leftText}>
                             Vote with us to find out the top 5 best quarintine movies
                         </Typography>
+                        <br/>
+                        <Grid item xs={12} lg={8} style={{marginRight: "5%"}}>
+                            <TextField 
+                        className={classes.searchBox}
+                        id="outlined-search" 
+                        label="Search and vote" 
+                        type="search" 
+                        variant="outlined" 
+                        fullWidth
+                        placeholder="Movie Name"
+                        helperText="This is a shopify project and not a real contest. Do not feel sad if your movie did not win."
+                        />
+                        </Grid>
+                        <Grid item xs={12} lg={2}>
+                            <Button 
+                            variant="contained" 
+                            disableElevation 
+                            className={classes.submitButton}
+                            inputLabelProps={{
+                                classes:{
+                                    root: classes.buttonStyle,
+                                    focused: classes.buttonStyle
+                                }
+                            }}
+                            inputProp={{
+                                className: {
+                                    root: classes.buttonStyle,
+                                    focused: classes.buttonStyle
+                                }
+                            }}
+                            >
+                                search
+                            </Button>
+                        </Grid>
+                        
                     </Grid>
-                    <Grid item className={classes.right} xs={12} lg={6}>
-                        <img src={stock} alt="image of two women voting" style={{maxHeight: "100%", maxWidth: "100%"}}/>
+                    <Grid item className={classes.right} xs={12} lg={6} style={{padding: "0", margin: "0"}}>
+                        <img src={stock} alt="image of two women voting" style={{maxHeight: "100%", maxWidth: "100%", width: "100%", height: "100%"}}/>
                     </Grid>
                 </Grid>
 
