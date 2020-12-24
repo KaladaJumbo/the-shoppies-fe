36acf764b7b6f5ca7f36fd3674bcea28f1489592
KaladaJumbo <wkalada1@gmail.com>
2020-12-23 21:02:07 -0500

    container added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react"
 import AppBar from "./components/appBar"
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+
+import Container from "@material-ui/core/Container";
+import { makeStyles } from '@material-ui/core/styles';
 
 import Welcome from "./pages/welcome"
 
@@ -9,17 +12,29 @@ import Welcome from "./pages/welcome"
 //go to http://www.omdbapi.com/ and request a key if you do not have one. 
 const OMDB_KEY = process.env.REACT_APP_OMDB_API_KEY;
 
-const App = () => {
+const useStyles = makeStyles((theme) => ({
+  root:{
+    margin: 0,
+    padding: 0
+    // backgroundColor: "#80deea"
+  },
+
+}));
+
+const App = props => {
+  const classes = useStyles();
 
   return (
     <div className="App">
       <Router>
           <AppBar />
-          <Switch>
-            <Route exact path="/">
-                <Welcome />
-            </Route>
-          </Switch>
+          <Container  maxWidth="xl" className={classes.root}>
+            <Switch>
+              <Route exact path="/">
+                  <Welcome />
+              </Route>
+            </Switch>
+          </Container>
       </Router>
     </div>
   );
--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Welcome = () => {
     return (
         <div>
-            
+            welcome page
         </div>
     )
 }
